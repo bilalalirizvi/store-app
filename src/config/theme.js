3b58d15391ac720962d8d4e73a02a3826ee85ce4
hamzakhandecotechs <bilalalirizvi5@gmail.com>
2023-10-17 17:58:47 +0500

    created: layout

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,3 +1,9 @@
-export const light = {};
+export const light = {
+  primary: "#7E549F",
+  siderBg: "#001529",
+};
 
-export const dark = {};
+export const dark = {
+  primary: "#7E549F",
+  siderBg: "#001529",
+};
